585493d71342cec835855d0c01364de0f5e626f0
cohoh <cohohohn04@gmail.com>
2022-05-10 21:12:20 +0200

    Change to Golden-Developer

--- src/main/java/de/goldendeveloper/ledercraftingrezept/Main.java
+++ src/main/java/de/goldendeveloper/ledercraftingrezept/Main.java
@@ -1,29 +1,27 @@
-package de.zrazzer.collincrafting;
+package de.goldendeveloper.ledercraftingrezept;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
-import org.bukkit.enchantments.Enchantment;
-import org.bukkit.event.Listener;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.Recipe;
 import org.bukkit.inventory.ShapedRecipe;
-import org.bukkit.inventory.ShapelessRecipe;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.ArrayList;
-import java.util.List;
+public final class Main extends JavaPlugin {
 
-public class RecipeLoader implements Listener {
-
-    public void registerRecipes(){
-
+    @Override
+    public void onEnable() {
         ItemStack leder = new ItemStack(Material.LEATHER);
         ItemMeta ledermeter = leder.getItemMeta();
         leder.setItemMeta(ledermeter);
         ShapedRecipe lederrezept = new ShapedRecipe(leder);
         lederrezept.shape("FNF", "FFF", "FFF");
         lederrezept.setIngredient('F', Material.ROTTEN_FLESH);
-
         Bukkit.addRecipe(lederrezept);
+    }
 
+    @Override
+    public void onDisable() {
     }
 }
